659814e17efef04cf65b9a7a9fab7cff1ef505c7
Nick Lindau <nicholaithegreat@gmail.com>
2021-03-22 10:06:19 -0700

    change startup.cs to reflect week 12 weekend HW

--- ProjectName/Startup.cs
+++ ProjectName/Startup.cs
@@ -42,13 +42,14 @@ namespace ProjectName
     public void Configure(IApplicationBuilder app)
     {
       app.UseDeveloperExceptionPage();
+      app.UseAuthentication();
       app.UseRouting();
-      app.UseStaticFiles();
+      app.UseAuthorization();   
       app.UseEndpoints(routes =>
       {
         routes.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
       });
-
+      app.UseStaticFiles();
       app.Run(async (context) =>
       {
         await context.Response.WriteAsync("Hello World!");
